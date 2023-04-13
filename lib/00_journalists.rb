bf3b770bd74a6f17e4a44997bfbfa320d0f173c7
Axeloow <axelouli45@gmail.com>
2023-04-13 14:04:11 +0200

    Ajout de l'exercice 0

---
+++ lib/00_journalists.rb
@@ -0,0 +1,90 @@
+journalistes = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+#On cherche la taille de l'array. On aurait également pu utiliser le .count
+#puts "L'array de journalistes contient #{journalistes.length} Handles."
+
+#Récupéré sur la documentation Ruby : Soit a et b les items 0 et 1 de l'array; la fonction compare (<=>) la taille (.length) et en ressort la plus petite (array.min)
+#puts "Le journaliste ayant l'handle le plus court est #{journalistes.min {|a, b| a.length <=> b.length}}."
+
+#On cherche les handles faisant une taille de 5 caractères. Le @ ne compte pas, on cherchera donc en réalité ceux qui en font 6.
+#Le .select permet de selectionner un groupe d'objet selon une condition spécifique. Ici, pour chaque élément de la liste dit "n", on regarde si leur longueur est égale à 6.
+#a=journalistes.select{|e| e.length==6}
+#puts "Le nombre de handles qui sont composés de 5 caractères est de #{a.length}."
+
+#Combien commencent par une majuscule ?
+#b=journalistes.select{|e| e[1] =~ /[A-Z]/}
+#puts "Le nombre de handles qui commencent par une majuscule est de #{b.count}."
+
+#Trie la liste par ordre alphabétique
+#puts "#{journalistes.sort_by(&:downcase) }"
+
+#Trie la liste par taille
+#puts "#{journalistes.sort_by{|e| e.length}}"
+
+#Sort la position de @epenser
+#puts "#{journalistes.index('@epenser')}"
+
+#Sort une répartition des handles par taille
+def taille(array)
+  lengthmax = array.max {|a, b| a.length <=> b.length}
+  max = lengthmax.length
+  lengthmin = array.min {|a, b| a.length <=> b.length}
+  min = lengthmin.length
+  rendu=[]
+  for n in min..max
+    stockage = [array.select{|e| e.length==n}]
+    if stockage.count > 0
+      puts "Voici les handles dont la longueur est de #{n} caractères :"
+      puts stockage
+    end
+  end
+end
+
+def perform(journalistes)
+  puts "Bonjour ! Nous avons crée une liste d'array, ainsi qu'un programme répondant à une liste définie de questions."
+  puts "Il vous suffit de sélectionner une question afin d'en avoir la réponse !"
+  puts "Appuyer sur Entrée pour passer à la liste des questions"
+  gets.chomp
+  puts "-- Liste de Questions --"
+  puts "0. Combien y a-t-il de handle dans cet array?"
+  puts "1. Quel est le handle le plus court de la liste?"
+  puts "2. Combien y a-t-il de handles contenant 5 caractères (le @ n'étant pas considéré comme un caractère)?"
+  puts "3. Combien commencent par une majuscule (première lettre juste après le @) ?"
+  puts "4. Trie la liste de handle par ordre alphabétique?"
+  puts "5. Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)"
+  puts "6. Quelle est la position dans l'array de la personne @epenser ?"
+  puts "7. Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)"
+  puts "-- Fin des Questions --"
+
+  input = gets.chomp
+  if input == "0"
+    #On cherche la taille de l'array. On aurait également pu utiliser le .count
+    puts "L'array de journalistes contient #{journalistes.length} Handles."
+  elsif input == "1"
+    #Récupéré sur la documentation Ruby : Soit a et b les items 0 et 1 de l'array; la fonction compare (<=>) la taille (.length) et en ressort la plus petite (array.min)
+    puts "Le journaliste ayant l'handle le plus court est #{journalistes.min {|a, b| a.length <=> b.length}}."
+  elsif input == "2"
+    a=journalistes.select{|e| e.length==6}
+    puts "Le nombre de handles qui sont composés de 5 caractères est de #{a.length}."
+  elsif input == "3"
+    b=journalistes.select{|e| e[1] =~ /[A-Z]/}
+    puts "Le nombre de handles qui commencent par une majuscule est de #{b.count}."
+  elsif input == "4"
+    puts "#{journalistes.sort_by(&:downcase) }"
+  elsif input == "5"
+    puts "#{journalistes.sort_by{|e| e.length}}"
+  elsif input == "6"
+    puts "La position de @epenser est #{journalistes.index('@epenser')}."
+  elsif input == "7"
+    number7 = taille(journalistes)
+  else
+    puts "Je n'ai pas compris..."
+  end
+end
+
+perform(journalistes)
+
+
+
+
+
